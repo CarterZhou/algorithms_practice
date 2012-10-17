8844d80bab2cc414b1aa10f7444941e09b631f07
Zhou Hao <zhou.hao.0112@gmail.com>
2012-10-17 12:26:42 +1100

    modified SearchLargestSmallerNumber.java

--- SearchLargestSmallerNumber.java
+++ SearchLargestSmallerNumber.java
@@ -2,41 +2,44 @@ package com.cupid.algorithm;
 
 public class SearchLargestSmallerNumber {
 
-	private static int binarySearchNearest(int[] array,int x){
+	private static int binarySearchNearest(int[] array, int x) {
 		int nearest = 0;
 		int low = 0;
-		int high = array.length-1;
+		int high = array.length - 1;
 		int mid = 0;
 		// Do binary search
-		while(low<=high){
-			mid = (low+high)/2;
+		while (low <= high) {
+			mid = (low + high) / 2;
 			// When low = high,
-			// an exact finish time may or may not be found.
-			// Assign value to highestCompatible accordingly.
-			if(low==high){
-				// e.g. find 15 between 14,16 and finish[mid] = 16.
+			// an exact number may or may not be found.
+			// Assign value to nearest accordingly.
+			if (low == high) {
+				// e.g. find 15 between 14,16 and array[mid] = 16.
 				// In this case return index of 14
-				if(x<array[mid]){
-					nearest = mid-1;
-				// e.g. find 15 between 14,16 and finish[mid] = 14.
-				// In this case return index of 14
-			    }else if(x>array[mid]){
-			    	nearest = mid;
+				if (x < array[mid]) {
+					nearest = mid - 1;
+					// e.g. find 15 between 14,16 and array[mid] = 14.
+					// In this case return index of 14
+				} else if (x > array[mid]) {
+					nearest = mid;
 				}
-			 }
-			if(x < array[mid]){
-				 high = mid-1;
-			}else if(x > array[mid]){
-				low = mid+1;
-			}else{
+			}
+			if (x < array[mid]) {
+				if(mid == 0){
+					nearest = -1;
+				}
+				high = mid - 1;
+			} else if (x > array[mid]) {
+				low = mid + 1;
+			} else {
 				// If array[mid] = x iterate backwards until the largest number < x is found.
-				mid = mid -1;
-				while(array[mid] == x && mid >=0){
-					mid = mid-1;
+				mid = mid - 1;
+				while (mid >= 0 && array[mid] == x) {
+					mid = mid - 1;
 				}
-				// If no such value is found, return the first number of array
-				if(mid <0){
-					mid = 0;
+				// If no such value is found, return the -1 indicating no value is less than x
+				if (mid < 0) {
+					mid = -1;
 				}
 				nearest = mid;
 				break;
@@ -44,11 +47,16 @@ public class SearchLargestSmallerNumber {
 		}
 		return nearest;
 	}
-	
+
 	public static void main(String[] args) {
-		int[] a = new int[]{0,4,5,6,7,9,9,9,9,10,11,12,14,16};
-		int index = binarySearchNearest(a, 1);
-		System.out.println("Index is " + index + " and value is " +  a[index]);
+		int[] a = new int[] { 3, 4, 5, 6, 7, 9, 9, 9, 9, 10, 11, 12, 14, 16 };
+		int x = 9;
+		int index = binarySearchNearest(a, x);
+		if(index==-1){
+			System.out.println("No value is less than " + x + " is found");
+		}else{
+			System.out.println("Index is " + index + " and value is " + a[index]);
+		}
 	}
 
 }
